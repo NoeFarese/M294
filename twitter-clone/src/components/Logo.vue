dd001396fabe9603c9d1ef4eab2c0fdb813185e7
Noé <110814367+NoeFarese@users.noreply.github.com>
2024-03-21 16:17:26 +0100

    add twitter-clone and tasks A1-A5 (Refactoring)

---
+++ twitter-clone/src/components/Logo.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+  <div class="logo">
+    Tw<strong>IT</strong>ter
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ twitter-clone/src/components/Navigation.vue
@@ -0,0 +1,20 @@
+<script setup>
+import IconLogin from "@/components/icons/IconLogin.vue";
+import IconHome from "@/components/icons/IconHome.vue";
+</script>
+
+<template>
+  <nav class="nav">
+    <a href="#" class="router-link-active">
+      <IconHome/>
+      Stream
+    </a>
+    <a href="#">
+      <IconLogin/>
+      Login
+    </a>
+  </nav>
+</template>
+
+<style scoped>
+</style>
