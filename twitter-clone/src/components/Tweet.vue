22030228a70b5c13de4402ab4985237d9b5feeed
Noé <110814367+NoeFarese@users.noreply.github.com>
2024-03-26 09:47:05 +0100

    add task D3

--- twitter-clone/src/components/Tweet.vue
+++ twitter-clone/src/components/Tweet.vue
@@ -1,21 +1,24 @@
 <script setup>
 
+const props = defineProps({
+  user: Object,
+  text: String,
+  created_at: String
+})
 </script>
 
 <template>
   <div class="tweet">
     <div class="tweet__avatar">
-      <img :src="`https://i.pravatar.cc/100?u=${tweet}`" alt=""/>
+      <img :src="`https://i.pravatar.cc/100?u=${props.user.id}`" alt=""/>
     </div>
     <div class="tweet__content">
       <div class="tweet__header">
-        <span class="tweet__author">Franzi Musterfrau</span>
-        <span class="tweet__timestamp">vor 1 Minute</span>
+        <span class="tweet__author">{{ props.user.name }}</span>
+        <span class="tweet__timestamp"> vor 1 Minute</span>
       </div>
       <div class="tweet__text">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam
-        tincidunt, nunc nisl aliquam nisl, eget aliquam nisl nunc vel nunc. Sed euismod, nunc ut
-        aliquam tincidunt, nunc nisl aliquam nisl, eget aliquam nisl nunc vel nunc.
+        {{ text }}
       </div>
     </div>
   </div>
--- twitter-clone/src/views/HomeView.vue
+++ twitter-clone/src/views/HomeView.vue
@@ -16,7 +16,12 @@ import LoginInfo from "@/components/LoginInfo.vue";
     <!-- Stream -->
     <section class="stream">
       <!-- Tweet -->
-      <Tweet v-for="tweet in [1, 2, 3, 4, 5, 6, 7, 8, 9 ]" />
+      <Tweet
+          v-for="tweet in [1, 2, 3, 4, 5, 6, 7, 8, 9]"
+          :user="{ id: 1, name: 'John Doe'}"
+          :text="'Lorem ipsum dolor med'"
+          :created-at="'2023-01-01T12:00:00Z'"
+       />
     </section>
   </main>
 </template>
