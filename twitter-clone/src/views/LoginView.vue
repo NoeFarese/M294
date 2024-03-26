f685ab3acad854fa515382d15ad48ea5ee24b9a1
Noé <110814367+NoeFarese@users.noreply.github.com>
2024-03-26 09:10:53 +0100

    add task D2

--- twitter-clone/src/views/LoginView.vue
+++ twitter-clone/src/views/LoginView.vue
@@ -1,23 +1,29 @@
+<script setup>
+import { ref } from 'vue';
+const email = ref('')
+const password = ref('')
+</script>
+
 <template>
   <div class="login">
     <section class="login-wrapper">
       <form action="#" class="login-form" autocomplete="off" novalidate>
         <div class="form-group">
           <label class="form-label" for="email">E-Mail</label>
-          <input class="form-input" type="email" id="email" />
+          <input class="form-input" type="email" id="email" v-model="email"/>
           <div class="form-error">
             Ungültige E-Mail
           </div>
         </div>
         <div class="form-group">
           <label class="form-label" for="password">Passwort</label>
-          <input class="form-input" type="password" id="password" />
+          <input class="form-input" type="password" id="password" v-model="password"/>
           <div class="form-error">
             Falsches Passwort
           </div>
         </div>
         <div class="form-group">
-          <button class="btn btn--primary btn--block">
+          <button :disabled="password === '' || email === ''" class="btn btn--primary btn--block">
             Login
           </button>
         </div>
